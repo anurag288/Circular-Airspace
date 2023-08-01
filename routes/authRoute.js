412f71165e0ada31087813755c465bafe3554220
Anurag <anuragsingh9643@gmail.com>
2023-08-02 02:40:50 +0530

    first commit

---
+++ routes/authRoute.js
@@ -0,0 +1,52 @@
+import express from "express";
+import {
+  registerController,
+  loginController,
+  testController,
+  updateProfileController,
+  getOrdersController,
+  getAllOrdersController,
+  orderStatusController,
+} from "../controllers/authController.js";
+import { isAdmin, requireSignIn } from "../middlewares/authMiddleware.js";
+
+const router = express.Router();
+
+// REGISTER || METHOD POST
+router.post("/register", registerController);
+
+// LOGIN || POST
+router.post("/login", loginController);
+
+// Test route
+router.get("/test", requireSignIn, isAdmin, testController);
+
+// Protected route
+router.get('/user-auth', requireSignIn, (req, res) => {
+  res.status(200).send({ ok: true });
+
+});
+// Protected route
+router.get('/admin-auth', requireSignIn, isAdmin, (req, res) => {
+  res.status(200).send({ ok: true });
+});
+
+//update profile
+router.put("/profile", requireSignIn, updateProfileController);
+
+
+//orders
+router.get("/orders", requireSignIn, getOrdersController);
+
+//all orders
+router.get("/all-orders", requireSignIn, isAdmin, getAllOrdersController);
+
+// order status update
+router.put(
+  "/order-status/:orderId",
+  requireSignIn,
+  isAdmin,
+  orderStatusController
+);
+
+export default router;
