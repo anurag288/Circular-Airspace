a145ff04bf554627867a15061b700e4a85585c7e
Anurag <72090477+anurag288@users.noreply.github.com>
2023-08-02 02:42:06 +0530

    Update server.js

--- server.js
+++ server.js
@@ -37,8 +37,8 @@ app.use("/api/v1/product", productRoutes);
 
 
 //rest api
-app.get("/", (req, res) => {
-  res.send("<h1>Welcome to ecommerce app</h1>");
+app.use('*', function (req, res) {
+    res.sendFile(path.join(__dirname, "./client/build/index.html"))
 });
 
   //PORT
